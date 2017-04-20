c11773089db3cb66e84c34f89a1451bcd4eaf1c3
Collin Rea <collinr01@gmail.com>
2017-04-20 06:30:58 -0400

    Initial Commit, Basic nav setup

---
+++ app/components/Main.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+
+var Nav = require('Nav');
+
+var Main = (props) => {
+  return (
+    <div>
+      <div>
+        <Nav />
+        <p>Main Component Rendered</p>
+        {props.children}
+      </div>
+    </div>
+  );
+};
+
+module.exports = Main;
